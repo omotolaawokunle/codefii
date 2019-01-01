fa57fb3d3e351712718a7bb3f7f5f1366c22329f
codefii <ekeminyd@gmail.com>
2019-01-01 23:37:37 +0100

    File uploader class added via Manage/File

---
+++ vendor/codefii/core/src/Network/Manager/File.php
@@ -0,0 +1,62 @@
+<?php
+namespace Codefii\Manager;
+class File {
+    private $supported_formats = array(
+        "image/png",
+        "image/jpeg",
+        "image/jpg",
+        "image/svg",
+        "image/gif"
+    );
+    public $file_name;
+
+    public $file_type;
+
+    public $file_temp;
+
+    public $file_size;
+
+    public $path;
+
+    public $isUploaded = false;
+    
+    public function upload(array $file){
+        $file_values = array_values($file);
+        $file_path = $file_values[0];
+        $this->path = $file_path;
+        $main_file = $file_values[1];
+        if(is_array($main_file)){
+            if(in_array($main_file['type'],$this->supported_formats)){
+                $this->file_name = $main_file['name'];
+                $this->file_type = $main_file['type'];
+                $this->file_temp = $main_file['temp'];
+                $this->file_size = $main_file['size'];
+                if($this->move()){
+                    $this->isUploaded = true;
+                }else{
+                    $this->isUploaded = false;
+                }
+            }else{
+                die("Format not supported");
+            }
+        }
+
+    }
+     public function getFileName(){
+        return $this->file_name;
+    }
+    public function getFileSize(){
+            return $this->file_size;
+    }
+        
+    public function move(){
+            return move_uploade_file($this->file_temp,
+            "/App/templates/{$this->path}/".$this->file_name);
+    
+    }
+    public function isUploaded(){
+        return $this->isUploaded;
+    }
+
+   
+}
